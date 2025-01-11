4e4622cbde2609f1c14b8b87951b590cfd497931
arshad300390 <arshad300390@gmail.comm>
2025-01-11 17:21:53 +0500

    fix params

--- src/app/(dashboard)/dashboard/users/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-
-// Define PageProps type with awaited params
-interface PageProps {
-  params: {
-    id: string;
-  };
-  searchParams: {
-    page: string;
-  };
-}
-
-// Define your Page component as async
-const Page = async ({ params, searchParams }: PageProps) => {
+const Page = async ({ params }: { params: { id: string } }) => {
   // Await params before accessing its properties
   const { id } = await params; // Awaiting params
-  const { page } = await searchParams; // Awaiting searchParams
 
   return (
     <div>
       <div>User: {id}</div>
-      <div>Page: {page}</div>
     </div>
   );
 };
