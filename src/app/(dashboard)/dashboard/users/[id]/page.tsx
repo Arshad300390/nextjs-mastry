75b6b76127f109d81ac90cd712412aa3777b08d1
arshad300390 <arshad300390@gmail.comm>
2025-01-11 17:14:06 +0500

    rm error

--- src/app/(dashboard)/dashboard/users/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-// Define PageProps type with an awaited params
+// Define PageProps type with awaited params
 interface PageProps {
   params: {
     id: string;
   };
+  searchParams: {
+    page: string;
+  };
 }
 
-const Page = async ({ params }: PageProps) => {
+// Define your Page component as async
+const Page = async ({ params, searchParams }: PageProps) => {
   // Await params before accessing its properties
   const { id } = await params; // Awaiting params
+  const { page } = await searchParams; // Awaiting searchParams
 
   return (
     <div>
       <div>User: {id}</div>
+      <div>Page: {page}</div>
     </div>
   );
 };
